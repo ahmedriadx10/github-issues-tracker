58b9b7fa880f0cf92c9f8dc35aacef61230fb7db
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-03-07 13:21:58 +0600

    issues card area layout made

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+const issuesCardContainer=elementTaker('issues-card-container')
+const loadingSpinner=elementTaker('spinner-container')
+const issuesCount=elementTaker('issues-count')
+const tabButtonsContainer=elementTaker('tab-buttons-container')
+const userSearchInput=elementTaker('user-search-input')
+const searchBtn=elementTaker('search-issues-btn')
+
+let issuesData=[]
+
