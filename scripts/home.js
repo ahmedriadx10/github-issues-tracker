fc0b93ea2a2b85b7026132a1989f5866ce3e6853
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-03-07 14:58:17 +0600

    all issue data got from API and UI made

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,134 @@
-const issuesCardContainer=elementTaker('issues-card-container')
-const loadingSpinner=elementTaker('spinner-container')
-const issuesCount=elementTaker('issues-count')
-const tabButtonsContainer=elementTaker('tab-buttons-container')
-const userSearchInput=elementTaker('user-search-input')
-const searchBtn=elementTaker('search-issues-btn')
+const issuesCardContainer = elementTaker("issues-card-container");
+const loadingSpinner = elementTaker("spinner-container");
+const issuesCount = elementTaker("issues-count");
+const tabButtonsContainer = elementTaker("tab-buttons-container");
+const userSearchInput = elementTaker("user-search-input");
+const searchBtn = elementTaker("search-issues-btn");
 
-let issuesData=[]
+let issuesData = [];
 
+async function getAllIssues() {
+  const dataGet = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const convData = await dataGet.json();
+
+  const { data } = convData;
+
+  issuesData = data;
+  renderIssuesUI(data);
+}
+
+function setPriorityBatch(priority) {
+  let priorityBatch = "";
+  switch (priority) {
+    case "high":
+      priorityBatch = `<div class="badge badge-soft badge-error uppercase font-medium">${priority}</div>`;
+      break;
+    case "medium":
+      priorityBatch = `<div class="badge badge-soft badge-warning uppercase font-medium">${priority}</div>`;
+
+      break;
+    case "low":
+      priorityBatch = `<div class="badge badge-soft badge-neutral/30 text-neutral/30 uppercase font-medium">${priority}</div>`;
+      break;
+
+    default:
+      priorityBatch = `<div class="badge badge-soft badge-neutral/30 text-neutral/30 uppercase font-medium">${"Unknown"}</div>`;
+  }
+
+  return priorityBatch;
+}
+
+function labelsSet(labels) {
+  const labelsCreate = labels.map((x) => {
+    if (x === "bug") {
+      return `  <div class=" rounded-full badge badge-soft badge-error uppercase font-medium"><i class="fa-solid fa-bug"></i> ${x}</div>`;
+    } else if (x === "help wanted") {
+      return `  <div class="rounded-full badge badge-soft badge-warning uppercase font-medium"><i class="fa-solid fa-life-ring"></i> ${x}</div>`;
+    } else if (x === "enhancement") {
+      return `  <div class="rounded-full badge badge-soft badge-success uppercase font-medium"><img src='./assets/Vector.png'> ${x}</div>`;
+    } else if (x === "documentation") {
+      return `  <div class="rounded-full badge badge-soft badge-primary uppercase font-medium"><i class="fa-solid fa-file-circle-exclamation"></i> ${x}</div>`;
+    } else if (x === "good first issue") {
+      return `  <div class="rounded-full badge badge-soft badge-info uppercase font-medium"><i class="fa-solid fa-thumbs-up"></i> ${x}</div>`;
+    }
+  });
+
+  const allLabels = labelsCreate.join(" ");
+  return allLabels;
+}
+
+function renderIssuesUI(getData) {
+  issuesCardContainer.innerHTML = "";
+
+  getData.forEach((x) => {
+    const {
+      id,
+      title,
+      description,
+      status,
+      labels,
+      priority,
+      author,
+      createdAt,
+    } = x;
+
+    const issueCard = document.createElement("div");
+
+    issueCard.innerHTML = `
+  
+  <div data-id='${id}' class="card bg-base-100 shadow-lg rounded-md    border-t-4 border-success w-full h-full">
+
+<!-- card content -->
+ <div class="p-4">  <!-- status and priority area -->
+<div class="flex justify-between items-center">
+  <img src="./assets/Open-Status.png" alt="">
+<div>${setPriorityBatch(priority)}</div>
+</div>
+
+<!-- card body area -->
+ <div class="mt-3"><h4 class="font-semibold text-neutral">${title}</h4>
+<p class="mt-2 text-[#64748B]">${description}</p>
+<!-- labels area  -->
+
+<div class="flex flex-wrap gap-y-2 gap-1.5  py-4 ">
+  ${labelsSet(labels)}
+</div>
+
+
+</div>
+
+</div>
+
+
+<div class="border-t p-4 border-[#E4E4E7]"><!-- author and date -->
+<p class="text-[#64748B]">${author}</p>
+<p class="text-[#64748B]">${createdAt.slice(0, 10)}</p></div>
+
+
+</div>
+  `;
+    issuesCardContainer.appendChild(issueCard);
+  });
+}
+
+getAllIssues();
+
+/**
+ * {
+    "id": 1,
+    "title": "Fix navigation menu on mobile devices",
+    "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+    "status": "open",
+    "labels": [
+        "bug",
+        "help wanted"
+    ],
+    "priority": "high",
+    "author": "john_doe",
+    "assignee": "jane_smith",
+    "createdAt": "2024-01-15T10:30:00Z",
+    "updatedAt": "2024-01-15T10:30:00Z"
+}
+ */
